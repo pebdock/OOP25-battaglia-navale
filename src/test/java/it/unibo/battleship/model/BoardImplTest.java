8af3a569175245388b2619ff4370504eb9606368
Marco Penolazzi <marco.penolazzi@studio.unibo.it>
2026-07-28 14:17:11 +0200

    Aggiunto test per BoardImpl che verifica il corretto funzionamento della nave da ricognizione, la sua forma a L e la rotazione di quest'ultima

--- src/test/java/it/unibo/battleship/model/BoardImplTest.java
+++ src/test/java/it/unibo/battleship/model/BoardImplTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.Test;
 // import it.unibo.battleship.model.visibility.OwnerOnlyVisibilityPolicy;
 
 import java.util.List;
-// import java.util.Set;
+import java.util.Set;
 
 /**
  * Tests teh board implementation.
@@ -35,5 +35,26 @@ class BoardImplTest {
         assertEquals(ShotOutcome.MISS, board.fireAt(List.of(new Coordinate(0, 0))).getFirst().outcome());
         assertEquals(ShotOutcome.HIT, board.fireAt(List.of(new Coordinate(1, 1))).getFirst().outcome());
         assertEquals(ShotOutcome.SUNK, board.fireAt(List.of(new Coordinate(1, 2))).getFirst().outcome());
+    }
+
+    /**
+     * Tests if the l shape remains if rotated.
+     */
+    @Test
+    void reconLShapeRotation() {
+        final Ship ship = Ship.place(
+            new ShipId("recon"),
+            ShipType.RECONNAISSANCE,
+            new Coordinate(4, 4),
+            Rotation.DEGREES_90
+        );
+
+        final int lValue = 5;
+
+        assertEquals(Set.of(
+            new Coordinate(4, lValue),
+            new Coordinate(lValue, lValue),
+            new Coordinate(lValue, 4)
+        ), ship.cells());
     }
 }
